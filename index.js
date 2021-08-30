218221234a080cf0243b0080e2fe211f991568a7
amitaibat97 <amitaibar97@gmail.com>
2021-08-30 21:03:53 +0300

    last fix(i hope)
    the thing with the median has been fixed, in case that the number of grades will be odd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const grades = [0,1,2,3,4,5,6,7,8,9]
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
 sortedGrades= grades.slice();
 sortedGrades.sort()
 const range= sortedGrades[(sortedGrades.length-1)]- sortedGrades[0];
@@ -6,8 +6,14 @@ console.log(grades)
 console.log(sortedGrades)
 console.log(range)
 
-const placeMid = Math.floor((sortedGrades.length)/2-1)
-const median= (sortedGrades[placeMid]+sortedGrades.length/2)/2
+
+if(grades.length%2==0){
+    median= (sortedGrades[(grades.length/2)-1]+ sortedGrades[sortedGrades.length/2])/2
+}else{
+    median= sortedGrades[(Math.floor(grades.length/2))]
+
+}
+
 console.log(median)
 
 const secHalf= grades.slice((grades.length)/2)
