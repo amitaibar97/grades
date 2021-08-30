4fcd7f2742e6767fe8af80f9fe6a4dc75a67563a
amitaibat97 <amitaibar97@gmail.com>
2021-08-30 19:49:45 +0300

    secHalf range
    did some changes with the names of arrays(sortedGrades in order to use the original grades list(not the sorted one) in that task...

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
-grades.sort();
-const range= grades[(grades.length-1)]- grades[0];
+sortedGrades= grades.slice();
+sortedGrades.sort()
+const range= sortedGrades[(sortedGrades.length-1)]- sortedGrades[0];
 console.log(grades)
+console.log(sortedGrades)
 console.log(range)
 
-const placeMid = Math.floor((grades.length)/2)
-const median= grades[placeMid]
+const placeMid = Math.floor((sortedGrades.length)/2-1)
+const median= sortedGrades[placeMid]
 console.log(median)
+
+const secHalf= grades.slice((grades.length)/2)
+secHalf.sort()
+halfRange= secHalf[secHalf.length-1]- secHalf[0]
+console.log(secHalf)
+console.log(halfRange)
