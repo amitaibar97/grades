d1001e90ae01c09a5ac02828620622c73f923472
amitaibat97 <amitaibar97@gmail.com>
2021-08-30 19:07:54 +0300

    median

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ grades.sort();
 const range= grades[(grades.length-1)]- grades[0];
 console.log(grades)
 console.log(range)
+
+const placeMid = Math.floor((grades.length)/2)
+const median= grades[placeMid]
+console.log(median)
